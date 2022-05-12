30ee8bb1d8edf519d19b9650809402c046a51e8d
Geoffrey Smith <smith.geoffrey.d@gmail.com>
2022-05-11 19:23:24 -0500

    updated three by one

--- Makefile
+++ Makefile
@@ -4,10 +4,13 @@ PORT=1337
 # DOCKER TASKS
 # Build the container
 build: ## Build the container
-	docker build -t $(APP_NAME) .
+	docker build --pull --rm -f "assets/Dockerfile" -t  $(APP_NAME):latest "assets"
 
 build-nc: ## Build the container without caching
-	docker build --no-cache -t $(APP_NAME) .
+	docker build --no-cache -t $(APP_NAME) ./assets/
 
 run:
-	docker run -i -t --rm -p=$(PORT):$(PORT) --name="$(APP_NAME)" $(APP_NAME)
+	docker run -i -t --rm -p=$(PORT):$(PORT) -v=${PWD}/app:/srv/app --name="$(APP_NAME)" dopomoha
+
+inspect:
+	docker exec -it ${APP_NAME} sh
--- assets/docker-entrypoint.sh
+++ assets/docker-entrypoint.sh
@@ -5,30 +5,18 @@ if [ "$*" = "strapi" ]; then
 
   if [ ! -f "package.json" ]; then
 
-    DATABASE_CLIENT=${DATABASE_CLIENT:-sqlite}
-
-    EXTRA_ARGS=${EXTRA_ARGS}
-
     echo "Using strapi $(strapi version)"
     echo "No project found at /srv/app. Creating a new strapi project ..."
 
-    DOCKER=true strapi new . --no-run \
-      --dbclient=$DATABASE_CLIENT \
-      --dbhost=$DATABASE_HOST \
-      --dbport=$DATABASE_PORT \
-      --dbname=$DATABASE_NAME \
-      --dbusername=$DATABASE_USERNAME \
-      --dbpassword=$DATABASE_PASSWORD \
-      --dbssl=$DATABASE_SSL \
-      --ts \
-      $EXTRA_ARGS
+    #DOCKER=true yarn create strapi-app@beta app \
+    #  --typescript \
 
   elif [ ! -d "node_modules" ] || [ ! "$(ls -qAL node_modules 2>/dev/null)" ]; then
 
     if [ -f "yarn.lock" ]; then
 
       echo "Node modules not installed. Installing using yarn ..."
-      yarn install --prod --silent
+      #yarn install --prod --silent
 
     else
 
@@ -46,7 +34,7 @@ if [ "$*" = "strapi" ]; then
   fi
 
   echo "Starting your app (with ${STRAPI_MODE:-develop})..."
-  exec strapi "${STRAPI_MODE:-develop}"
+  #exec strapi "${STRAPI_MODE:-develop}"
 
 else
   exec "$@"
